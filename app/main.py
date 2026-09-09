a16c219d8ce0045d07aa9144d89d732d0ab1b8f5
mostafa2k <m2kappswindows8.1@gmail.com>
2026-09-09 13:34:31 +0330

    feat(app): enhance FastAPI initialization with description and version

--- app/main.py
+++ app/main.py
@@ -3,7 +3,11 @@ from fastapi import FastAPI
 from app.api.auth import router as auth_router
 from app.api.posts import router as post_router
 
-app = FastAPI(title="FastAPI Blog")
+app = FastAPI(
+    title="FastAPI Blog",
+    description="A simple blog API built with FastAPI and SQLAlchemy",
+    version="1.0.0",
+)
 
 
 app.include_router(auth_router)
